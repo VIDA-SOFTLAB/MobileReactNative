93b242ffd61153766f66134d63118679908c0a52
Mtsj8 <marcelojunior.tranche@gmail.com>
2022-12-12 00:18:13 -0300

    fix(firebaseOption): Deixando criador no fire

--- src/helper/db.ts
+++ src/helper/db.ts
@@ -5,42 +5,41 @@ import {createUser} from './firebase'
 const HOST = 'http://localhost:5000'
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIiLCJqdGkiOiI5NjdmMzBlNy05NDZhLTRkOTYtYTY4ZC1lYTYwMDhhMmM4MTQiLCJOYW1lSWQiOiIxIiwiYWRtaW5pc3RyYXRvck5vdyI6IjEiLCJleHAiOjE2NzYzODA2MzksImlzcyI6ImlyaXMyLnNAZW1haWwuY29tIiwiYXVkIjoiaXJpczIuc0BlbWFpbC5jb20ifQ.CtDvxFEzP-XYGXPOmECig93E4T8N1tGYETXNB4AHRkg'
 
-export async function register(data){
-  const user = await createUser(data.Email, data.Password)
+export async function register(data, createFirebase = true){
+  if (createFirebase){
+    const user = await createUser(data.Email, data.Password)
+    const userInfo = user.userInfo
 
-  if (user.status){
-      const userInfo = user.userInfo
+    console.log(userInfo.uid)
 
-      console.log(userInfo.uid)
+    //data.uid = userInfo.uid
+  }
+  if (!(user.status)){
+    return {
+      status : false,
+      error  : user.errorCode
+    }
+  }
 
-      //data.uid = userInfo.uid
+  var options = {
+      'method': 'POST',
+      'url': HOST + '/api/1/patient/',
+      'headers': {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + TOKEN
+      },
+      body: JSON.stringify(data)
+  };
 
-      var options = {
-          'method': 'POST',
-          'url': HOST + '/api/1/patient/',
-          'headers': {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + TOKEN
-          },
-          body: JSON.stringify(data)
-      };
-
-      return new Promise(function(resolve, reject) {
-          request.post(options, function(err, resp, body) {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(body);
-            }
-          })
+  return new Promise(function(resolve, reject) {
+      request.post(options, function(err, resp, body) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(body);
+        }
       })
-  }
-  else{
-      return {
-          status : false,
-          error  : user.errorCode
-      }
-  }
+  })
 }
 
 export async function getUserByCpf(cpf){
